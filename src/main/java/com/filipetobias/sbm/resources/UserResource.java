28933d1eb5abdba5b091fb9635fd4ecf8e49c350
filipe.tobias <filipe.tobias@senior.com.br>
2019-01-31 14:45:52 -0200

    Obtendo um usuário por id

--- src/main/java/com/filipetobias/sbm/resources/UserResource.java
+++ src/main/java/com/filipetobias/sbm/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.filipetobias.sbm.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,13 @@ public class UserResource {
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        List<UserDTO> listDto = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable  String id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
--- src/main/java/com/filipetobias/sbm/services/UserServiceImpl.java
+++ src/main/java/com/filipetobias/sbm/services/UserServiceImpl.java
@@ -2,10 +2,12 @@ package com.filipetobias.sbm.services;
 
 import com.filipetobias.sbm.domain.User;
 import com.filipetobias.sbm.repository.UserRepository;
+import com.filipetobias.sbm.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -16,5 +18,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public User findById(String id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
     }
 }
